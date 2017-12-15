2534a629ddc836b9ec1798ec7d152590166f7834
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2017-12-15 14:16:54 +0000

    Minor text changes

--- ahds/__init__.py
+++ ahds/__init__.py
@@ -16,12 +16,15 @@ class AmiraFile(object):
         self.__fn = fn
         self.__header = header.AmiraHeader.from_file(self.__fn, *args, **kwargs)
         self.__data_streams = None # only populate on call to read() method
+        
     @property
     def header(self):
         return self.__header
+    
     @property
     def data_streams(self):
         return self.__data_streams
+    
     def read(self, *args, **kwargs):
         self.__data_streams = data_stream.DataStreams(self.__fn, *args, **kwargs)
         
