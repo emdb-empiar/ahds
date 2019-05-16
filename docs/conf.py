791f72dd883f3d6cf2fb81d89e6214338758df0f
Paul K. Korir <31738053+paulkorir@users.noreply.github.com>
2019-05-16 11:15:42 +0100

    Trying another fix for mocking (see https://docs.readthedocs.io/en/stable/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules for details of using autodoc_mock_imports option)

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,8 @@
 
 import sys
 import os
-import mock
 
-MOCK_MODULES = ['simpleparse.parser']
-
-for mod_name in MOCK_MODULES:
-    print "Mocking {}...".format(mod_name)
-    sys.modules[mod_name] = mock.Mock()
+autodoc_mock_imports = ['simpleparse']
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
