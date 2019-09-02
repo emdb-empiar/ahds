69eb493a4c036ebc1cf50acc770f9f82f84471cd
paulkorir <pkorir@ebi.ac.uk>
2019-09-02 15:53:16 +0100

    Updates to install_requires

--- setup.py
+++ setup.py
@@ -42,26 +42,51 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
-setup(
-    name="ahds",
-    version=AHDS_VERSION,
-    packages=find_packages(),
-    author="Paul K. Korir, PhD",
-    author_email="pkorir@ebi.ac.uk, paul.korir@gmail.com",
-    description="Python package to parse and provide access to headers and data streams in Amira (R) files",
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    url="https://github.com/emdb-empiar/ahds.git",
-    license="Apache License",
-    keywords="header, parser, data streams",
-    setup_requires=["numpy"],
-    # additional dependencies to prevent failed install due to no support for Py27
-    install_requires=["simpleparse>=2.1.1", "scikit-image"],
-    ext_modules=[decoders],
-    include_dirs=[np.get_include()],
-    entry_points={
-                'console_scripts': [
-                      'ahds = ahds.ahds:main',
-                ]
-          }
-)
+if sys.version_info[0] > 2:
+    setup(
+        name="ahds",
+        version=AHDS_VERSION,
+        packages=find_packages(),
+        author="Paul K. Korir, PhD",
+        author_email="pkorir@ebi.ac.uk, paul.korir@gmail.com",
+        description="Python package to parse and provide access to headers and data streams in Amira (R) files",
+        long_description=long_description,
+        long_description_content_type='text/x-rst',
+        url="https://github.com/emdb-empiar/ahds.git",
+        license="Apache License",
+        keywords="header, parser, data streams",
+        setup_requires=["numpy"],
+        # additional dependencies to prevent failed install due to no support for Py27
+        install_requires=["simpleparse>=2.1.1", "scikit-image"],
+        ext_modules=[decoders],
+        include_dirs=[np.get_include()],
+        entry_points={
+            'console_scripts': [
+                'ahds = ahds.ahds:main',
+            ]
+        }
+    )
+else:
+    setup(
+        name="ahds",
+        version=AHDS_VERSION,
+        packages=find_packages(),
+        author="Paul K. Korir, PhD",
+        author_email="pkorir@ebi.ac.uk, paul.korir@gmail.com",
+        description="Python package to parse and provide access to headers and data streams in Amira (R) files",
+        long_description=long_description,
+        long_description_content_type='text/x-rst',
+        url="https://github.com/emdb-empiar/ahds.git",
+        license="Apache License",
+        keywords="header, parser, data streams",
+        setup_requires=["numpy"],
+        # additional dependencies to prevent failed install due to no support for Py27
+        install_requires=["simpleparse==2.1.1", "scikit-image<0.14", "scipy<1.2"],
+        ext_modules=[decoders],
+        include_dirs=[np.get_include()],
+        entry_points={
+                    'console_scripts': [
+                          'ahds = ahds.ahds:main',
+                    ]
+              }
+    )
